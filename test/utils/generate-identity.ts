c3b1d0d885b4ac33e05fa807b877a63db26314c9
ZenVoich <zen.voich@gmail.com>
2023-06-22 15:52:27 +0400

    add test workflow

--- test/utils/generate-identity.ts
+++ test/utils/generate-identity.ts
@@ -4,10 +4,10 @@ import { Secp256k1KeyIdentity } from '@dfinity/identity-secp256k1';
 // generate random identity
 // use seed to generate deterministic identity
 export let generateIdentity = (seed?: string) => {
-   if (seed) {
+   if (seed !== undefined) {
       return Secp256k1KeyIdentity.generate(Buffer.from([...Buffer.from(seed, 'utf8'), ...Array(32).fill(0)].slice(0, 32)));
    }
-   else if (seed === '') {
+   else if (seed === '' || seed === null) {
       return new AnonymousIdentity;
    }
    return Secp256k1KeyIdentity.generate();
